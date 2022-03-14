a98a91391f6e38d17250c65ac4c62d056ce34c64
Robin Bette <robinbette@hotmail.com>
2022-03-14 14:33:10 +0100

    Try out provider based filtering

--- CarvedRock.Api/Program.cs
+++ CarvedRock.Api/Program.cs
@@ -1,7 +1,14 @@
 using CarvedRock.Data;
 using CarvedRock.Domain;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+var tracePath = Path.Join(path, $"Log_CarvedRock_{DateTime.Now.ToString("yyyyMMdd-HHmm")}.txt");
+
+Trace.Listeners.Add(new TextWriterTraceListener(System.IO.File.CreateText(tracePath)));
+Trace.AutoFlush = true;
 
 // Services
 builder.Services.AddControllers();
