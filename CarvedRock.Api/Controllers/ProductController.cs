e46ff6a6908a9ffa5103ca2b0d45d2a3100c153d
Robin Bette <robinbette@hotmail.com>
2022-03-14 13:35:48 +0100

    Add updated starter project with data project

--- CarvedRock.Api/Controllers/ProductController.cs
+++ CarvedRock.Api/Controllers/ProductController.cs
@@ -1,5 +1,5 @@
+using CarvedRock.Data.Entities;
 using CarvedRock.Domain;
-using CarvedRock.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarvedRock.Api.Controllers;
@@ -8,16 +8,34 @@ namespace CarvedRock.Api.Controllers;
 [Route("[controller]")]
 public class ProductController : ControllerBase
 {
+    private readonly ILogger<ProductController> _logger;
     private readonly IProductLogic _productLogic;
 
-    public ProductController(IProductLogic productLogic)
+    public ProductController(ILogger<ProductController> logger, IProductLogic productLogic)
     {
+        _logger = logger;
         _productLogic = productLogic;
     }
 
     [HttpGet]
-    public async Task<IEnumerable<ProductModel>> Get(string category = "all")
+    public async Task<IEnumerable<Product>> Get(string category = "all")
     {
-        return await _productLogic.GetProductsForCategory(category);
+        _logger.LogInformation("Getting products in API for {category}", category);
+        return await _productLogic.GetProductsForCategoryAsync(category);
+        //return _productLogic.GetProductsForCategory(category);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get(int id)
+    {
+        //var product = await _productLogic.GetProductByIdAsync(id);
+        var product = _productLogic.GetProductById(id);
+        if (product != null)
+        {
+            return Ok(product);
+        }
+        return NotFound();
     }
 }
